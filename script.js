611a1077289bd655d81af4371837443b3b047c92
Chris Fink <65560083+chris-fink@users.noreply.github.com>
2022-09-06 15:36:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-https://api.github.com/users
+//variables for API
+const url = 'https://api.github.com/users/chris-fink';
+const main = document.querySelector('main');
+const search = document.querySelector('#search');
+const form = document.querySelector('form');
+
+//Fetch API data
+function searchProfile(url){
+    fetch(url)
+    .then(res => res.json())
+    .then(function(data){
+
+        //Create elements for HTML response
+            const img = document.createElement('img');
+            const h3 = document.createElement('h3');
+            const h2 = document.createElement('h2');
+            const p = document.createElement('p');
+
+        //Append elements to the main elements
+            main.appendChild(img);
+            main.appendChild(h3);
+            main.appendChild(h2);
+            main.appendChild(p);
+            img.src = `${data.avatar_url}`;
+
+            //Display API data on elements
+            h3.innerHTML = ` Visit GitHub : <a href="${data.html_url}" target="_blank">${data.login}</a>`;
+            h2.innerHTML = `${data.name}`;
+            p.innerHTML = `${data.public_repos} Repositories`;
+    });
+}
+
+//Creating search form
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    main.innerHTML = '';
+
+    const searchTerm = search.value;
+    if (searchTerm) {
+        searchProfile('https://api.github.com/users/' + searchTerm);
+        search.value = '';
+    }
+});
